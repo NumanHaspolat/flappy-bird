408b62acf7af6bd0647c05492ba2f84712634ded
Numan Haspolat <haspolatnuman@gmail.com>
2023-11-29 19:08:31 +0300

    ffss

--- app.js
+++ app.js
@@ -63,7 +63,9 @@ window.onload = function () {
   setInterval(placePipes, 1500); //every 1.5 seconds
   document.addEventListener("keydown", moveBird);
   document.addEventListener("touchstart", function (event) {
-    moveBird();
+    if (event.touches[0].pageX > window.innerWidth / 2) {
+      moveBird();
+    }
   });
 };
 
